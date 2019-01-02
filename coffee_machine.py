c9558da02c1867c34b66859d09ca9c66b3259ab1
Maciej Szumowski <mpszumowski@gmail.com>
2019-01-02 16:40:57 +0100

    procedure notes

--- coffee_machine.py
+++ coffee_machine.py
@@ -2,6 +2,7 @@ from inspect import getmembers, isclass, isabstract
 
 import coffees
 from dregs_container import DregsContainer
+from exceptions import CoffeeMachineException
 from water_supply import WaterSupply
 
 
@@ -42,6 +43,30 @@ class CoffeeMachine(object):
         for name, klass in classes:
             if isclass(klass) and issubclass(klass, coffees.AbcCoffee):
                 self.coffee_programs.update({name: klass})
+
+    def prepare(self, program):
+        coffee_program = self.coffee_programs.get(program)
+        if coffee_program is None:
+            raise CoffeeMachineException(
+                "{} program has not been found. Most probably it has not been programmed.".format(program))
+        """
+        - coffee program has information about a) order of ingredients; b) amount of each ingredient
+        - each ingredient requires a component linked with CoffeeMachine
+            * coffee - grinder, dregs_container
+            * milk - milk_pump
+            * water - water_supply
+                grinded_coffee = self.grinder.get_coffee(coffee_ingredient.amount / 2, num_espresso_units)  # espresso ratio 1 g coffee : 2 ml of water
+                water = self.water_supply.get_water(coffee_ingredient.amount)
+                self.extract_coffee(grinded_coffee, water)
+                self.dregs_container.store(grinded_coffee)
+                ###
+                milk = self.milk_pump.get_milk(milk_ingredient.amount)
+                self.add_coffee(milk)
+                ###
+                water = self.water_supply.get_water(water_ingredient.amount)
+                self.add_water(water)
+        - 
+        """
 
     def is_ready(self):
         return all(

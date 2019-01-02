fbe8a6348ca1373086c7ede191326ce48e21007b
Maciej Szumowski <mpszumowski@gmail.com>
2019-01-02 14:11:51 +0100

    coffee factory: load coffee programs

---
+++ coffee_machine.py
@@ -0,0 +1,23 @@
+from inspect import getmembers, isclass, isabstract
+import coffees
+
+
+class CoffeeMachine(object):
+    coffee_programs = {}  # TODO: add typing - key: str, val: class
+
+    def __init__(self):
+        self.is_ready()  # TODO: check if machine is ready
+        self._load_coffee_programs()
+
+    def _load_coffee_programs(self):
+        classes = getmembers(coffees,
+                             lambda c: isclass(c) and not isabstract(c))
+        for name, klass in classes:
+            if isclass(klass) and issubclass(klass, coffees.AbcCoffee):
+                self.coffee_programs.update({name: klass})
+
+    def is_ready(self):
+        # TODO: check water
+        # TODO: check grains
+        # TODO: check dregs container
+        pass

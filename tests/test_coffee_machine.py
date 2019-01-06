ec24260695edf85fbaa752c4215672436525bfb0
Maciej Szumowski <mpszumowski@gmail.com>
2019-01-06 23:02:30 +0100

    tests: test initialization of main class

---
+++ tests/test_coffee_machine.py
@@ -0,0 +1,17 @@
+import unittest
+
+from coffee_machine.coffee_machine import CoffeeMachine
+from coffee_machine.components import WaterLine
+
+
+class TestInitialization(unittest.TestCase):
+
+    def test_initialization(self):
+        machine = CoffeeMachine()
+
+    def test_init_waterline(self):
+        machine = CoffeeMachine(water_supply=WaterLine)
+
+
+if __name__ == '__main__':
+    unittest.main()

8f85723304de9acfd4145b68500a6a6a14aab2e9
Maciej Szumowski <mpszumowski@gmail.com>
2019-01-14 16:44:29 +0100

    create unified volume property in RefillableContainer class without setter

--- coffee_machine/components.py
+++ coffee_machine/components.py
@@ -39,8 +39,9 @@ class CoffeeMachineComponent(metaclass=ABCMeta):
 
 class RefillableContainer(object):
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, volume, *args, **kwargs):
         self._level = 0
+        self._volume = volume
         super(RefillableContainer, self).__init__(*args, **kwargs)
 
     @property
@@ -52,7 +53,14 @@ class RefillableContainer(object):
         if amount < 0:
             raise ValueError('Level of Refillable Container subclass: '
                              '{} cannot be lower than 0'.format(self.__class__))
+        if amount > self._volume:
+            raise ValueError('Level of Refillable Container subclass: '
+                             '{} cannot exceed its volume'.format(self.__class__))
         self._level = amount
+
+    @property
+    def volume(self):
+        return self._volume
 
     def add(self, amount):
         self.level += amount
@@ -66,11 +74,11 @@ class WaterTank(RefillableContainer, CoffeeMachineComponent):
     warning_message = 'Water low: refill water tank'
 
     def __init__(self, owner):
-        super().__init__(owner)
         config = get_config()
         params = get_params()
         self.warning_level = config['WaterTank']['warning_level']
-        self.volume = params['WaterTank']['size']
+        volume = params['WaterTank']['size']
+        super().__init__(owner=owner, volume=volume)
         print('Water tank connected')
 
     def __str__(self):
@@ -98,20 +106,20 @@ class CoffeeGrinder(RefillableContainer, CoffeeMachineComponent):
     warning_message = 'Coffee low: refill coffee grinder'
 
     def __init__(self, owner):
-        super().__init__(owner)
         config = get_config()
         params = get_params()
         self.warning_level = config['CoffeeGrinder']['warning_level']
-        self.capacity = params['CoffeeGrinder']['size']
+        volume = params['CoffeeGrinder']['size']
+        super().__init__(owner=owner, volume=volume)
         print('Coffee grinder is up...')
 
     def __str__(self):
         return 'Coffee Grinder component. Capacity: {} | Current level: {}'.format(
-            self.capacity, self.level
+            self.volume, self.level
         )
 
     def refill(self):
-        amount = self.capacity - self.level
+        amount = self.volume - self.level
         super().add(amount)
         self.health()
         print('Coffee grinder refilled.')
@@ -122,7 +130,7 @@ class CoffeeGrinder(RefillableContainer, CoffeeMachineComponent):
         return amount
 
     def is_ready(self):
-        return self.level > self.capacity * self.warning_level
+        return self.level > self.volume * self.warning_level
 
 
 class DregsContainer(RefillableContainer, CoffeeMachineComponent):
@@ -130,17 +138,16 @@ class DregsContainer(RefillableContainer, CoffeeMachineComponent):
     warning_message = 'Dregs container full: empty container'
 
     def __init__(self, owner):
-        super().__init__(owner)
         config = get_config()
         params = get_params()
         self.warning_level = config['DregsContainer']['warning_level']
-        self.max_volume = params['DregsContainer']['size']
-        self._level = 0
+        volume = params['DregsContainer']['size']
+        super().__init__(owner=owner, volume=volume)
         print('Dregs container connected...')
 
     def __str__(self):
         return 'Dregs container component. Capacity: {} | Current level: {}'.format(
-            self.max_volume, self.level
+            self.volume, self.level
         )
 
     def empty(self):
@@ -154,7 +161,7 @@ class DregsContainer(RefillableContainer, CoffeeMachineComponent):
         self.health()
 
     def is_ready(self):
-        return self.level < self.max_volume * self.warning_level
+        return self.level < self.volume * self.warning_level
 
 
 class Brewer(CoffeeMachineComponent):
@@ -162,7 +169,7 @@ class Brewer(CoffeeMachineComponent):
     warning_message = 'Unexpected malfunction'
 
     def __init__(self, owner):
-        super().__init__(owner)
+        super().__init__(owner=owner)
         print('Brewer is up...')
 
     def __str__(self):
@@ -187,7 +194,7 @@ class MilkPump(CoffeeMachineComponent):
     warning_message = 'Milk is not supplied'
 
     def __init__(self, owner):
-        super().__init__(owner)
+        super().__init__(owner=owner)
         self.milk_supply = None
         print('Milk pump is ready...')
 
